ce0e2fe03cfa5dc5c4219d243506cc08523f96a5
TMR2005 <148249199+TMR2005@users.noreply.github.com>
2024-12-07 10:35:38 +0530

    Update Teams.cpp

--- Teams.cpp
+++ Teams.cpp
@@ -8,3 +8,28 @@ void Team::displayPlayers() {
 bool Team::isAllOut() {
     return wickets == players.size();
 }
+string Team::getTopScorer() {
+    int maxRuns = 0;
+    string topScorer;
+    for (auto p : players) {
+        batsmen* b = dynamic_cast<batsmen*>(p);
+        if (b && b->getRuns() > maxRuns) {
+            maxRuns = b->getRuns();
+            topScorer = b->getName();
+        }
+    }
+    return topScorer;
+}
+
+string Team::getTopWicketTaker() {
+    int maxWickets = 0;
+    string topWicketTaker;
+    for (auto p : players) {
+        bowler* b = dynamic_cast<bowler*>(p);
+        if (b && b->getWickets() > maxWickets) {
+            maxWickets = b->getWickets();
+            topWicketTaker = b->getName();
+        }
+    }
+    return topWicketTaker;
+}
