4df261e3ffbd2308edfc340004c997986123ded6
TMR2005 <148249199+TMR2005@users.noreply.github.com>
2024-12-07 10:03:43 +0530

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,3 +1,8 @@
 #include "player.hpp"
 
-
+friend istream& operator >> (istream& i,batsmen& b){
+cout<<"Enter the name of the player";
+i>>b.name;
+    
+friend ostream& operator << (ostream&,batsmen&){
+  
