726a203d5cda30f78544765dcd797ba11de80b90
TMR2005 <148249199+TMR2005@users.noreply.github.com>
2024-12-07 10:05:16 +0530

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -4,5 +4,9 @@ friend istream& operator >> (istream& i,batsmen& b){
 cout<<"Enter the name of the player";
 i>>b.name;
     
-friend ostream& operator << (ostream&,batsmen&){
+friend ostream& operator << (ostream& o,batsmen& b){
+     o << "Batsman: " << b.name << " | Runs: " << b.runs << " | Balls: " << b.balls 
+             << " | SR: " << b.strikeRate << " | Fours: " << b.fours 
+             << " | Sixes: " << b.sixes << " | Out: " << (b.wicket ? "Yes" : "No") << endl;
+}
   
